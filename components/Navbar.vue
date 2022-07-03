4cdf815d147cd853c68bfb8260c0a09f4534b36c
CodeCasterNL <github@codecaster.nl>
2022-07-03 17:45:14 +0200

    Initial Docs commit

---
+++ components/Navbar.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+const { navigation } = useContent()
+</script>
+
+<template>
+  <div class="xl:container mx-auto flex justify-between px-4 py-4 sm:px-8">
+    <!-- Navigation -->
+    <div class="text-gray-700 dark:text-gray-200">
+      <NuxtLink
+        v-for="link of navigation"
+        :key="link._path"
+        :to="link._path"
+        active-class="font-bold"
+        class="mr-4"
+      >
+        {{ link.navTitle || link.title }}
+      </NuxtLink>
+    </div>
+    <!-- Social icons & Color Mode -->
+    <div class="space-x-3 text-gray-500 transition">
+      <a href="https://github.com/PDFinch/Client-NET" title="GitHub: PDFinch.com .NET/C# Client" class="hover:text-gray-700 dark:hover:text-gray-300"><Icon name="fa-brands:github" /></a>
+      <ColorModeSwitch class="hover:text-gray-700 dark:hover:text-gray-300" />
+    </div>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="xl:container mx-auto max-w-2xl px-4 py-10 bg-white sm:px-8 sm:shadow dark:bg-gray-800 sm:rounded">
+    <main class="prose prose-gray dark:prose-invert max-w-5xl">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style lang="postcss">
+/* Customize headers to remove default underline */
+.prose h2 a,
+.prose h3 a {
+  @apply no-underline;
+  &:hover {
+    @apply border-b border-neutral-600 dark:border-neutral-300;
+  }
+}
+</style>
